95a8f79fde40ca7c9a549ae5deb587f2554e57d6
shangliy <kudoysl@gmail.com>
2015-12-06 01:07:44 -0800

    Final parameter selection and training
    Shanglin & Yi:Implement cross-validation find parameters implement final
    training and test; upload results to Kaggle.
    accuracy improve to 0.8(validation)

---
+++ 12_4/Upc_first_num.m
@@ -0,0 +1,32 @@
+N1 = size(IG_new,1);
+max_per_row = zeros(N1,1);
+for i=1:1:N1
+   i
+   max_per_row(i) = max(IG_new(i,:));
+end
+disp('finished')
+%%
+N2 = 5000;
+order_de = sort(max_per_row,'descend');
+ind = find(max_per_row>=order_de(N2),N2);
+%%
+order_upc = unique(Upc_new);
+order_book = Upc_new(ind);
+%%
+N3 = size(X,1);
+X_Upc_5000 = zeros(N3,N2);
+for i=1:1:N3
+  i
+  for j=1:1:209
+      if(X(i,j)~=0)
+      ind = find(X(i,j)==order_book);
+         if isempty(ind) 
+         else
+         X_Upc_5000(i,ind)=1;
+         end
+      else
+         break;
+      end
+  end
+end
+disp('finished')
